72d3f2c883b29bb0aac5afaece80a2790150ee74
us0ldx <98390771+us0ldx@users.noreply.github.com>
2022-04-28 18:23:06 +0700

    Add files via upload

---
+++ js/cookie.js
@@ -0,0 +1,4 @@
+function getCookie(name){let matches=document.cookie.match(new RegExp("(?:^|; )"+name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g,'\\$1')+"=([^;]*)"));return matches?decodeURIComponent(matches[1]):undefined;}
+if(!document.cookie||!getCookie("language")||!getCookie("pass_survey")||!getCookie("cookies")){document.cookie="language=ru"
+document.cookie="pass_survey=false"
+document.cookie="cookies=false"}
